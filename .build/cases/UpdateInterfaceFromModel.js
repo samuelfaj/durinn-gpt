59cbec2954a7235083032af4326d29e50280e074
Samuel Faj <samuelfaj@icloud.com>
2023-04-06 17:22:49 -0300

    :)

--- .build/cases/UpdateInterfaceFromModel.js
+++ .build/cases/UpdateInterfaceFromModel.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const PromptForCode_1 = __importDefault(require("../classes/PromptForCode"));
 const Default_BaseModel_1 = __importDefault(require("../defaults/Default.BaseModel"));
+const Default_Interface_1 = __importDefault(require("../defaults/Default.Interface"));
 class UpdateInterfaceFromModel extends PromptForCode_1.default {
 }
 UpdateInterfaceFromModel.description = `Atualiza uma interface com base em um model que passamos.`;
@@ -13,9 +14,13 @@ UpdateInterfaceFromModel.prompt = `Vou fornecer alguns exemplos para que você g
     `\`\`\`\n` +
     `${Default_BaseModel_1.default}\n` +
     `\`\`\`\n\n` +
-    `Esse é o model do nosso sistema:\n\n` +
+    `Esse é um exemplo de interface padrão do nosso sistema:\n\n` +
+    `\`\`\`\n` +
+    `${Default_Interface_1.default}\n` +
+    `\`\`\`\n\n` +
+    `Esse é o model a ser analisado:\n\n` +
     `\`\`\`\n` +
     `{{CODE-OR-FILE}}\n` +
     `\`\`\``;
-UpdateInterfaceFromModel.ask = `Com base no model acima, atualize a interface abaixo para que seja fiel ao model, você deve responder em markdown apenas o novo código e entre (\`\`\`). Sem explicações. Apenas o novo código.\n\`\`\`\n{{SAVE-TO-FILE}}\`\`\``;
+UpdateInterfaceFromModel.ask = `Com base no model acima, atualize a interface abaixo para que contenha todas as colunas e relações do model, você deve responder em markdown apenas o novo código e entre (\`\`\`). Sem explicações. Apenas o novo código.\n\`\`\`\n{{SAVE-TO-FILE}}\`\`\``;
 exports.default = UpdateInterfaceFromModel;
--- src/cases/UpdateInterfaceFromModel.ts
+++ src/cases/UpdateInterfaceFromModel.ts
@@ -2,6 +2,7 @@ import DurinnGPT from "../classes/DurinnGPT";
 import Api from "../classes/Api";
 import PromptForCode from "../classes/PromptForCode";
 import DefaultBaseModel from "../defaults/Default.BaseModel";
+import DefaultInterface from "../defaults/Default.Interface";
 
 export default class UpdateInterfaceFromModel extends PromptForCode {
 	static description = `Atualiza uma interface com base em um model que passamos.`;
@@ -11,9 +12,13 @@ export default class UpdateInterfaceFromModel extends PromptForCode {
 		`\`\`\`\n`+ 
 		`${DefaultBaseModel}\n`+ 
 		`\`\`\`\n\n` + 
-		`Esse é o model do nosso sistema:\n\n` + 
+		`Esse é um exemplo de interface padrão do nosso sistema:\n\n`+ 
+		`\`\`\`\n`+ 
+		`${DefaultInterface}\n`+ 
+		`\`\`\`\n\n` + 
+		`Esse é o model a ser analisado:\n\n` + 
 		`\`\`\`\n` + 
 		`{{CODE-OR-FILE}}\n` + 
 		`\`\`\``;
-	static ask = `Com base no model acima, atualize a interface abaixo para que seja fiel ao model, você deve responder em markdown apenas o novo código e entre (\`\`\`). Sem explicações. Apenas o novo código.\n\`\`\`\n{{SAVE-TO-FILE}}\`\`\``;
+	static ask = `Com base no model acima, atualize a interface abaixo para que contenha todas as colunas e relações do model, você deve responder em markdown apenas o novo código e entre (\`\`\`). Sem explicações. Apenas o novo código.\n\`\`\`\n{{SAVE-TO-FILE}}\`\`\``;
 }
