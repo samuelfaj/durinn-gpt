e67d42566bcae4dbab76cf0eda87df339f654d47
Samuel Faj <samuelfaj@icloud.com>
2023-04-08 18:29:26 -0300

    :)

--- .build/cases/EditModel.js
+++ .build/cases/EditModel.js
@@ -94,9 +94,10 @@ Esse é o model do nosso sistema:
     }
     static createMigration(toDo, modelPath, verbose = false) {
         return __awaiter(this, void 0, void 0, function* () {
-            const modelDir = fs.existsSync(modelPath)
-                ? modelPath
-                : path.resolve(process.env.PWD, modelPath);
+            const modelDir = fs.existsSync(path.resolve(process.env.PWD, modelPath))
+                ? path.resolve(process.env.PWD, modelPath)
+                : modelPath;
+            console.log('modelDir', modelDir);
             const array = modelDir.split('/');
             const modelName = array.pop().replace('.ts', '');
             let i = 0;
--- src/cases/EditModel.ts
+++ src/cases/EditModel.ts
@@ -77,9 +77,11 @@ Esse é o model do nosso sistema:
     }
 
     protected static async createMigration(toDo: string, modelPath: string, verbose = false){
-        const modelDir = fs.existsSync(modelPath) 
-        ? modelPath 
-        : path.resolve(process.env.PWD, modelPath);
+        const modelDir = fs.existsSync(path.resolve(process.env.PWD, modelPath)) 
+        ? path.resolve(process.env.PWD, modelPath)  
+        : modelPath;
+
+        console.log('modelDir', modelDir);
 
         const array = modelDir.split('/');
         const modelName = array.pop().replace('.ts', '');
